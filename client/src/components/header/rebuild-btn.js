84c0fe393741911f86d03d0f65960a56e1001d72
Maksym Dukov <maxopowerful@gmail.com>
2020-10-29 22:41:15 +0200

    - rebuild btn - add success visual status

--- client/src/components/header/rebuild-btn.js
+++ client/src/components/header/rebuild-btn.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useApiCall } from "hooks/use-api-call";
 import { rebuildFrontendApi } from "./rebuild.api";
 import WithCenteredLoader from "components/loader/with-centered-loader";
 import { Button, makeStyles } from "@material-ui/core";
+import clsx from "clsx";
 
 const useStyles = makeStyles(({ palette }) => ({
   rebuild: {
@@ -12,24 +13,31 @@ const useStyles = makeStyles(({ palette }) => ({
   btn: {
     marginLeft: "auto",
   },
+  rebuildSuccess: {
+    backgroundColor: palette.success.main,
+  },
 }));
 
 const RebuildBtn = () => {
   const classes = useStyles();
+  const [published, setPublished] = useState(false);
   const { trigger, loading, data } = useApiCall({
     fetcher: rebuildFrontendApi,
+    onSuccess: () => {
+      setPublished(true);
+    },
   });
   console.log("data", data);
 
   return (
     <WithCenteredLoader loading={loading} wrapperClass={classes.btn}>
       <Button
-        className={classes.rebuild}
+        className={clsx(classes.rebuild, published && classes.rebuildSuccess)}
         onClick={trigger}
-        disabled={loading}
+        disabled={loading || published}
         title="Перестроить весь сайт"
       >
-        Опубликовать
+        {published ? "Публикуется... ожидайте" : "Опубликовать"}
       </Button>
     </WithCenteredLoader>
   );
