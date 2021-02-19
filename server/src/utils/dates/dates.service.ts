3550b391bb555b5bf5bb6887cd9525ae1e0c6fe2
Maksym Dukov <maxopowerful@gmail.com>
2021-02-19 16:11:18 +0200

    - DAY_IN_MILLISECONDS const

--- server/src/utils/dates/dates.service.ts
+++ server/src/utils/dates/dates.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
+const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+
 @Injectable()
 export class DatesService {
   diffInDays(date1: Date, date2: Date) {
-    return (date2.getTime() - date1.getTime()) / (1000 * 60 * 60 * 24);
+    return (date2.getTime() - date1.getTime()) / DAY_IN_MILLISECONDS;
   }
 }
